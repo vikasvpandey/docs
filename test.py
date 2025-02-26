bbffe62234df9c5b516b7981ee3943a55e005632
vikasvpandey <vikaspandey3682@gmail.com>
2025-02-26 14:55:24 +0530

    Fix for issue #107

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-#this is a test py file
+# This is a test py file
 def sum_num(num1, num2):
-  return num1+num2
-result=sum_num(10)
+    return num1 + num2
+
+result = sum_num(10, 5)  # Provide two arguments instead of one
 print(result)
