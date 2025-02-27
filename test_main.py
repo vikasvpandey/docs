553b085f724689101db3b5e33ae6e98916885429
vikasvpandey <vikaspandey3682@gmail.com>
2025-02-27 16:34:10 +0530

    Add tests for issue #165

---
+++ test_main.py
@@ -0,0 +1,23 @@
+"""
+Tests for main.py
+"""
+import unittest
+from main import *
+
+class TestMain(unittest.TestCase):
+    def setUp(self):
+        """Set up test fixtures"""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures"""
+        pass
+
+    def test_ask_question(self):
+        """Test ask_question functionality"""
+        # TODO: Implement test case
+        self.assertTrue(True)  # Placeholder assertion
+
+
+if __name__ == "__main__":
+    unittest.main()
