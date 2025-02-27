41c90dd9c96b15cfaea061f259b938b875498b0a
vikasvpandey <vikaspandey3682@gmail.com>
2025-02-27 14:15:54 +0530

    Fix for issue #126

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import chainlit as cl
 import logging
+import chainlit as cl
+import logging
 from appconfig import AppConfig
 from azureai import AzureAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -563,4 +565,144 @@ async def plot_charts(df, chart_type):
                 cl.Plotly(name="chart", figure=fig, display="inline", size="large")
             ]
         ).send()
+
+
+async def process_idea():
+    print("Processing user idea.")
+    await send_message("Thank you for sharing your idea.")
+    response = await ask_action("Are you ready to answer a few questions to understand the case?", ["yes", "no"])
+    if response == "yes":
+        runnable = cl.user_session.get("runnable")
+        # Use await for asynchronous execution to ensure completion
+        content = await runnable.ainvoke({"input": user_idea})
+        try:
+            cleaned_content = content.strip().strip('```json').strip('```').strip()
+            questions = json.loads(cleaned_content)
+            print(f"Questions generated: {questions}")
+            print("User is ready to answer questions.")
+            await ask_question(0, questions)
+        except json.JSONDecodeError as e:
+            logging.error(f"Error decoding JSON: {e}")
+            questions = []
+    else:
+        global ans
+        ans = await create_business_case()
+        save_case = await ask_action("Would you like to save your report or refine them a bit further?", ["I would like to save", "Let me ask somethings a bit further"])
+        if save_case == "I would like to save":
+            global item_id
+            item_id = await save_case_generated(user_idea, ans, item_id)
+            await send_message('Your Case has been saved') if item_id is not None else send_message('There has been an error saving your message, hold on...')
+    def clean_numeric(cls, value):
+        if isinstance(value, str):
+            #remove $ and , characters
+            cleaned_value = value.replace("$", "").replace(",", "").strip()
+            
+            # Handling percentages by removing % and converting to float
+            if "%" in cleaned_value:
+                return float(cleaned_value.replace("%", "").strip())
+            try:
+                ##convert the clean value to an int
+                return int(cleaned_value)
+            except ValueError:
+                raise ValueError(f"Invalid cost value: {value}")
+        return value
+
+class RoiTableList(BaseModel):
+    __root__: List[RoiTableSchema]
+
+
+async def parse_markdown_to_dataframe(response: str):
+    """
+    Parses a markdown response into a DataFrame.
+
+    Args:
+        response (str): The markdown text to parse.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the parsed data.
+
+    Raises:
+        ValidationError: If the response fails validation.
+        Exception: For any other processing errors.
+    """
+    # Define the PydanticOutputParser and PromptTemplate logic here
+    parser = PydanticOutputParser(pydantic_object=RoiTableList)
+
+    prompt = PromptTemplate(
+        template=(
+            "Extract the ROI Analysis table from the markdown input and return each row as JSON."
+            "Ensure the keys match this structure: 'Category', 'Component', 'Cost', 'Calculations', 'Assumptions'."
+            "Ensure numeric fields (Cost, Break_even_months) are integers without formatting (no $ or commas).\n"
+            "{markdown_input}\n"
+        ),
+        input_variables=["markdown_input"],
+    )
+
+    # Use global ai_model instead of model
+    chain = prompt | ai_model | parser
+
+    try:
+        # Process the markdown input through the chain
+        result = await chain.ainvoke({"markdown_input": response})
+        
+        # Ensure the result is a list
+        if isinstance(result, dict):
+            result = [result]  # Wrap single dict in a list
+
+
+        # Convert result to a DataFrame
+        data = [item.dict() for item in result.__root__]
+        df = pd.DataFrame(data)
+        print("dataframe:",df)
+        return df
+    except ValidationError as e:
+        print("Validation error:", e)
+        return pd.DataFrame()  # Return an empty DataFrame in case of validation errors
+
+    except Exception as e:
+        print("Error:", e)
+        return pd.DataFrame()  # Return an empty DataFrame in case of other errors
+
+
+
+###############################################################################################
+
+##creating plotly graphs
+
+async def plot_charts(df, chart_type):
+    """
+    Generate Plotly charts (bar, line, pie) based on the provided DataFrame.
+    """
+    print("plot chart started")
+    if chart_type == 'bar':
+        filtered_df = df[~df['Category'].isin(['Breakeven Analysis', 'ROI Calculation'])]
+        fig = px.bar(
+            filtered_df,
+            x='Category',
+            y='Cost',
+            color='Component',
+            title='Cost Distribution by the Category and Component',
+            text='Cost',
+            barmode='group',
+            text_auto=True,
+        )
+        print("before fig update")
+        fig.update_layout(
+            yaxis_title="Cost ($)",
+            xaxis_title="Category",
+            legend_title='Component'
+        )
+        fig.update_traces(
+            width=0.60,
+            opacity=0.8
+        )
+        
+        # Send chart as a separate message without content
+        await cl.Message(
+            content="",  # Empty content to avoid expandable section
+            elements=[
+                cl.Plotly(name="chart", figure=fig, display="inline", size="large")
+            ]
+        ).send()
+
 
